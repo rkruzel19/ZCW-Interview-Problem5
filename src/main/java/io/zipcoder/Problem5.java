76af599d38bd554ba44bc609c1be68e2c80e96f1
rob <rob@zipcoders-MacBook-Pro.local>
2017-11-28 10:39:31 -0500

    first commit

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,24 @@
 package io.zipcoder;
 
 public class Problem5 {
+
+    public static void main(String[] args) {
+        String input = "I love hockey";
+        System.out.println(reverseString(input));
+    }
+
+    public static String reverseString(String string){
+        String output = "";
+        for (int i = string.length() - 1; i >= 0; i--){
+                output += string.charAt(i);
+        }
+
+        for (int i = 0; i < output.length(); i++){
+            if (i == 0 || output.charAt(i) == ' '){
+                
+            }
+        }
+        return output;
+    }
+
 }
