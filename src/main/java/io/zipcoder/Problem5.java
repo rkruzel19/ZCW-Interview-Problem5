324066ff9a2c7eaa4e090800dd818f1b24b7d386
rkruzel19 <rkruzel576@gmail.com>
2018-03-04 17:30:50 -0500

    spring cleaning. changed test back to original

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -3,21 +3,22 @@ package io.zipcoder;
 public class Problem5 {
 
     public static void main(String[] args) {
-        String input = "I love hockey";
+        String input = "I love UNIT Testing";
         System.out.println(reverseString(input));
     }
 
     public static String reverseString(String string){
         String output = "";
-        for (int i = string.length() - 1; i >= 0; i--){
-                output += string.charAt(i);
-        }
-
-        for (int i = 0; i < output.length(); i++){
-            if (i == 0 || output.charAt(i) == ' '){
-                
+        if (string.length() % 2 != 0){
+            for (int i = 0; i < (string.length() / 2); i++){
+                string = string.charAt(string.length() - 1 - i) + string.;
             }
         }
+
+//        for (int i = 0; i < string.length(); i--){
+//                output += string.charAt(i);
+//        }
+
         return output;
     }
 
